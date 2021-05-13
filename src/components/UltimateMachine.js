37396946a0749f1e6235379d87744f52fb1a65ba
Minhvn98 <Minhvn62@wru.vn>
2021-05-13 08:30:11 +0700

    If part 02

--- src/components/UltimateMachine.js
+++ src/components/UltimateMachine.js
@@ -1,14 +1,9 @@
 import React, { useState } from 'react';
 import './UltimateMachine.css';
 
-const OnMessage = () => <span>The machine is ON</span>;
+const OnMessage = () => <span>The machine is ON1</span>;
 
 const OffMessage = () => <span>The machine is OFF</span>;
-
-const OnOff = ({ isOn }) => {
-  if (isOn) return <OnMessage></OnMessage>;
-  return <OffMessage></OffMessage>;
-};
 
 const UltimateMachine = () => {
   const [isOn, setIsOn] = useState(false);
@@ -16,10 +11,15 @@ const UltimateMachine = () => {
     setIsOn(!isOn);
   };
 
+  const getMessage = () => {
+    if (isOn) return <OnMessage></OnMessage>;
+    return <OffMessage></OffMessage>;
+  };
+
   return (
     <section>
       <h1>Ultimate Machine</h1>
-      <OnOff isOn={isOn} />
+      {getMessage()}
       <button type="button" onClick={onClickHandler} aria-pressed={isOn}>
         On/Off
       </button>
