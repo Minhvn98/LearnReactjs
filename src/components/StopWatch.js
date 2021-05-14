2a46b97183d413c65b520a4e458e775d1de048bd
Minhvn98 <Minhvn62@wru.vn>
2021-05-14 08:48:03 +0700

    38. useEffect and closure

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -1,17 +1,15 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 const StopWatch = () => {
   const [time, setTime] = useState(0);
   const [counterActive, setCounterActive] = useState(true);
 
-  const currentTime = useRef(0);
-
+  const fomattedTime = new Date(time * 1000).toISOString().substr(11, 8);
   useEffect(() => {
     let interval = null;
 
     if (counterActive) {
       interval = setInterval(() => {
-        currentTime.current++;
-        setTime(currentTime.current);
+        setTime((t) => t + 1);
         console.log('Set time');
       }, 1000);
     }
@@ -26,7 +24,7 @@ const StopWatch = () => {
   return (
     <div>
       <h1>Stop Watch</h1>
-      <p>{time}</p>
+      <p>{fomattedTime}</p>
       <button onClick={onClickHandler} aria-pressed={counterActive}>
         Stop/Start
       </button>
