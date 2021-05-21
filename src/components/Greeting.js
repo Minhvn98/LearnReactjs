7bc547ae89f64eb36c81b507d6e0b7dd18640e52
Minhvn98 <minhvn62@wru.vn>
2021-05-21 14:34:13 +0700

    01.useState

---
+++ src/components/Greeting.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const Greeting = ({ initialName = '' }) => {
+  const [name, setName] = useState(initialName)
+
+  const handleChange = e => {
+    setName(e.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input id="name" name="name" onChange={handleChange} value={name} />
+      </form>
+      {name ? <strong>Hello {name} </strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+export default Greeting
